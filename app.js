3b780faacfdaea089478fa81a49b914cba65ed95
Imran Hasan <imranhasan871@gmail.com>
2022-07-09 12:24:11 +0600

    decrease function

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-function handelProductChange(isIncrease) {
-    const caseInput = document.getElementById('case-count');
+function handelProductChange(product, isIncrease) {
+    const caseInput = document.getElementById(product + '-count');
     const caseCount = parseInt(caseInput.value);
     let caseNewCount = caseCount;
     if (isIncrease == true) {
@@ -11,22 +11,5 @@ function handelProductChange(isIncrease) {
     caseInput.value = caseNewCount;
 
     const caseTotal = caseNewCount * 59;
-    document.getElementById('case-total').innerHTML = '$ ' + caseTotal;
-}
-
-function handlePhoneChange(isIncrease) {
-    const phoneInput = document.getElementById('phone-count');
-    const phoneCount = parseInt(phoneInput.value);
-    let phoneNewCount = phoneCount;
-    if (isIncrease == true) {
-        phoneNewCount = phoneCount + 1;
-    }
-
-    if (isIncrease == false && phoneCount > 0) {
-        phoneNewCount = phoneCount - 1;
-    }
-    phoneInput.value = phoneNewCount;
-
-    const phoneTotal = phoneNewCount * 1219;
-    document.getElementById('phone-total').innerHTML = '$ ' + phoneTotal;
+    document.getElementById(product + '-total').innerText = '$ ' + caseTotal;
 }
