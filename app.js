5a5506ca4ae38293c070916a0227f4c0e09acd26
Imran Hasan <imranhasan871@gmail.com>
2022-07-09 14:08:54 +0600

    fix floting problem in tax section

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 function handelProductChange(product, isIncrease) {
-    const productInput = document.getElementById(product + '-count');
-    const productCount = parseInt(productInput.value);
+    const productCount = getInputValue(product);
+
     let productNewCount = productCount;
+
     if (isIncrease == true) {
         productNewCount = productCount + 1;
     }
     if (isIncrease == false && productCount > 0) {
         productNewCount = productCount - 1;
     }
-    productInput.value = productNewCount;
+
+    document.getElementById(product + '-count').value = productNewCount;
 
     let productNewTotal = 0;
     if (product == 'case') {
@@ -20,15 +22,26 @@ function handelProductChange(product, isIncrease) {
 
     document.getElementById(product + '-total').innerText =
         '$ ' + productNewTotal;
+    handelCheckout();
 }
 
 function handelCheckout() {
-    const caseInput = document.getElementById('case-count');
-    const caseCount = parseInt(caseInput.value);
-    const phoneInput = document.getElementById('phone-count');
-    const phoneCount = parseInt(phoneInput.value);
+    const phoneCount = getInputValue('phone');
+
+    const caseCount = getInputValue('case');
 
     const totalPrice = caseCount * 59 + phoneCount * 1219;
     document.getElementById('total-price').innerText = '$ ' + totalPrice;
 
+    const tax = Math.round(totalPrice * .1);
+    document.getElementById('tax-amount').innerText = '$ ' + tax;
+
+    const grandTotal = totalPrice + tax;
+    document.getElementById('grand-total').innerText = '$ ' + grandTotal;
+}
+
+function getInputValue(product) {
+    const productInput = document.getElementById(product + '-count');
+    const productCount = parseInt(productInput.value);
+    return productCount;
 }
