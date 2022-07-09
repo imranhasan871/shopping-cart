907cdd0eb2d489edbfb1014920f270240707380e
Imran Hasan <imranhasan871@gmail.com>
2022-07-09 12:04:43 +0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## shopping Cart
+
+    []: # Language: Html
+    []: # Path: index.html
+
+    []: # Language: Css
+    []: # Path: style.css
+
+    []: # Language: JavaScript
+    []: # Path: app.js
+
+## Description
+
+It is a shopping cart application. I created this application to learn how to use the shopping cart.
+It has the following features: product list, shopping cart, checkout, and payment.
+You can add products to the shopping cart, remove products from the shopping cart, and checkout.
+You can also pay for your order. You can also see the total price of your order. You can also see the total price of your order.
+
+## Live Demo
+
+## Screenshots
---
+++ app.js
@@ -0,0 +1,32 @@
+function handelProductChange(isIncrease) {
+    const caseInput = document.getElementById('case-count');
+    const caseCount = parseInt(caseInput.value);
+    let caseNewCount = caseCount;
+    if (isIncrease == true) {
+        caseNewCount = caseCount + 1;
+    }
+    if (isIncrease == false && caseCount > 0) {
+        caseNewCount = caseCount - 1;
+    }
+    caseInput.value = caseNewCount;
+
+    const caseTotal = caseNewCount * 59;
+    document.getElementById('case-total').innerHTML = '$ ' + caseTotal;
+}
+
+function handlePhoneChange(isIncrease) {
+    const phoneInput = document.getElementById('phone-count');
+    const phoneCount = parseInt(phoneInput.value);
+    let phoneNewCount = phoneCount;
+    if (isIncrease == true) {
+        phoneNewCount = phoneCount + 1;
+    }
+
+    if (isIncrease == false && phoneCount > 0) {
+        phoneNewCount = phoneCount - 1;
+    }
+    phoneInput.value = phoneNewCount;
+
+    const phoneTotal = phoneNewCount * 1219;
+    document.getElementById('phone-total').innerHTML = '$ ' + phoneTotal;
+}
